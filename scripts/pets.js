6e5bac5a6b819718c12b726d772e37fe11c0902a
SamIslam2000 <zmnobab1999@gmail.com>
2024-10-12 18:28:35 +0600

    pets individual category added

--- scripts/pets.js
+++ scripts/pets.js
@@ -1,4 +1,4 @@
-const loadCategories = () => {
+const loadCategory = () => {
     fetch("https://openapi.programming-hero.com/api/peddy/categories")
         .then((res) => res.json())
         .then((data) => displayCategories(data.categories))
@@ -7,81 +7,65 @@ const loadCategories = () => {
 const loadPets = () => {
     fetch("https://openapi.programming-hero.com/api/peddy/pets")
         .then((res) => res.json())
-        .then((data) => displayPets(data.pets))
+        .then((data) => displayPetsCard(data.pets))
         .catch((error) => console.log(error));
 };
-// {
-//     "petId": 1,
-//     "breed": "Golden Retriever",
-//     "category": "Dog",
-//     "date_of_birth": "2023-01-15",
-//     "price": 1200,
-//     "image": "https://i.ibb.co.com/p0w744T/pet-1.jpg",
-//     "gender": "Male",
-//     "pet_details": "This friendly male Golden Retriever is energetic and loyal, making him a perfect companion for families. Born on January 15, 2023, he enjoys playing outdoors and is especially great with children. Fully vaccinated, he's ready to join your family and bring endless joy. Priced at $1200, he offers love, loyalty, and a lively spirit for those seeking a playful yet gentle dog.",
-//     "vaccinated_status": "Fully",
-//     "pet_name": "Sunny"
-// }
+const loadCategoryPets = (id) => {
+    fetch(`https://openapi.programming-hero.com/api/peddy/category/${id}`)
+        .then((res) => res.json())
+        .then((data) => displayPetsCard(data.data))
+        .catch((error) => console.log(error));
+};
 const displayCategories = (categories) => {
-    categories.forEach((item) => {
-        const categoryContainer = document.getElementById("categories")
-        console.log(categories);
-        const button = document.createElement("button");
-        button.classList = "btn"
-        button.innerText = item.category;
-        categoryContainer.append(button)
+    const categoryContainer = document.getElementById('categories');
+    categories.forEach(item => {
+        const btn = document.createElement('button');
+        btn.classList = 'bg-gray-100';
+        btn.innerHTML = `<button onclick="loadCategoryPets('${item.category}')" id="${item.category}" class="flex justify-between px-4 py-3 items-center font-bold gap-2">
+                            <img class="w-[20px]" src="${item.category_icon}"/> ${item.category}
+                         </button>`;
+        categoryContainer.append(btn);
     });
 };
-// const cardDemo=[
-//     {
-//         "id": 1,
-//         "category": "Cat",
-//         "category_icon": "https://i.ibb.co.com/N7dM2K1/cat.png"
-//     },
-//     {
-//         "id": 2,
-//         "category": "Dog",
-//         "category_icon": "https://i.ibb.co.com/c8Yp1y7/dog.png"
-//     },
-//     {
-//         "id": 3,
-//         "category": "Rabbit",
-//         "category_icon": "https://i.ibb.co.com/3hftmLC/rabbit.png"
-//     },
-//     {
-//         "id": 4,
-//         "category": "Bird",
-//         "category_icon": "https://i.ibb.co.com/6HHZwfq/bird.png"
-//     }
-// ]
-const displayPets = (pets) => {
-    const petsContainer = document.getElementById("pets");
-    pets.forEach(pet => {
-        const card = document.createElement("div");
-        card.classList="card card-compact";
-        card.innerHTML =
-        `
-        <figure class="h-[200px]">
-            <img
-            src="${pet.image}"
-            class="h-full w-full rounded-xl object-cover"
-            alt="Shoes" />
-        </figure>
-        <div class="px-0 py-2">
-            <h2 class="card-title">${pet.pet_name}</h2>
-            <p>Breed:${pet.breed?.length>=0? `${pet.breed}`:"Not Available"}</p>
-            <p>Birth:${pet.date_of_birth?.length>=0 ? `${pet.date_of_birth}`: "Not Available"}</p>
-            <p>Gender:${pet.gender?.length>=0 ? `${pet.gender}`: "Not Available"}</p>
-            <p>Price:${pet.price>0 ? `${pet.price}$`:"Not Available"}</p>
-            <div class="card-actions">
-            <button class="btn"><i class="fa-regular fa-thumbs-up"></i></button>
-            <button class="btn">Adopt</button>
-            <button class="btn">Details</button>
-            </div>
+const displayPetsCard = (pets) => {
+    const allPetsContainers = document.getElementById('pets');
+    allPetsContainers.innerHTML = "";
+    if(pets.length == 0){
+        allPetsContainers.innerHTML = `
+        <div>
+        <img src="images/error.webp" alt="">
+        <h2>No Information Available</h2>
+        <p>This subject-related information is not available.</p>
         </div>
         `;
-        petsContainer.append(card);
+        return;
+    }
+    pets.forEach((pet) => {
+        const card = document.createElement('div');
+        card.classList = 'card card-compact py-3';
+        card.innerHTML = `
+        <div class="card card-compact bg-base-100 w-3/4 md:w-80 lg:w-96 mx-auto shadow-xl px-10 py-5">
+                    <figure class ="w-full" relative">
+                      <img
+                        src= ${pet.image}
+                        alt="Pets" />
+                    </figure>
+                    <div class="">
+                      <h2 class="card-title">${pet.pet_name}</h2>
+                        <p>Breed:${pet.breed?.length >= 0 ? `${pet.breed}` : "Not Available"}</p>
+                        <p>Birth:${pet.date_of_birth?.length >= 0 ? `${pet.date_of_birth}` : "Not Available"}</p>
+                        <p>Gender:${pet.gender?.length >= 0 ? `${pet.gender}` : "Not Available"}</p>
+                        <p>Price:${pet.price > 0 ? `${pet.price}$` : "Not Available"}</p>
+                        <div class="card-actions">
+                        <button class="btn"><i class="fa-regular fa-thumbs-up"></i></button>
+                        <button class="btn">Adopt</button>
+                        <button class="btn">Details</button>
+                        </div>
+                    </div>
+                  </div>
+                `
+        allPetsContainers.append(card);
     })
-};
-loadCategories()
-loadPets()
+}
+loadPets();
+loadCategory();
